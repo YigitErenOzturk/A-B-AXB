6e5066000a7711e69fb86f3e10f96ea7a1296750
oztur <oztur@YIIT>
2022-11-22 12:07:28 +0100

    Proje dosyası ekle.

---
+++ Ders/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Rakam A");
+float a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Rakam B");
+float b = Convert.ToInt32(Console.ReadLine());
+double sonuc = (a * b) / (a + b);
+Console.WriteLine("A+B/AXB=");
+Console.WriteLine(sonuc);
+
+
